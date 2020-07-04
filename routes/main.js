799c085e20a86aa9fde9dec55ff1422613d26586
Yeon <jryoun1@naver.com>
2020-07-04 13:21:04 +0000

    회원탈퇴부분 구현

--- routes/main.js
+++ routes/main.js
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
 });
 
 //가게 등록하는 부분
-router.post('/registore',function(req,res){
+router.post('/info/registore',function(req,res){
     const ownerEmail = req.body.ownerEmail;
     const ownerStoreName = req.body.ownerStoreName;
     const ownerStoreAddress = req.body.ownerStoreAddress;
@@ -42,7 +42,31 @@ router.post('/registore',function(req,res){
 });
 
 
+//회원탈퇴부분
+router.post('/info/withdraw',function(req,res){
+    const ownerEmail = req.body.ownerEmail;
+    const ownerWithdraw = true; //회원탈퇴하면 true값으로 변경 default = 0(false)
+    const trashOwnerEmail = '0@0' //탈퇴하면 email중복검사에서 해당 email을 사용할 수 있게 하기 위해 trash email id 입력
+    const sql = 
+    'update Owners set IsOwnerDeleted = ? , OwnerEmail =? where OwnerEmail = ?';
+    const params = [ownerWithdraw, trashOwnerEmail, ownerEmail];
 
+    connection.query(sql, params,function(err,result){
+        let resultCode = 500;
+        let message = "Server Error";
+
+        if(err){
+            console.log(err);
+        }else{
+            resultCode = 201;
+            message = 'Account Deleted';    
+        }
+        res.json({
+            'code' : resultCode,
+            'message' : message
+        });
+    });
+});
 
 //무엇을 export할지를 결정하는것 
 module.exports = router;
